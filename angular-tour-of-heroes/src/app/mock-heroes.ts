4913ba6e6bef3385664ad67e47735db92af86095
ap080221063 <ap080221063@gmail.com>
2018-02-18 23:22:54 +0000

    listing heroes! global vars export and import to components, *ngFor binding

---
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[] = [
+    {id: 1, name: 'Superman'},
+    {id: 2, name: 'Batman'},
+    {id: 3, name: 'Spiderman'},
+    {id: 4, name: 'Thor'},
+    {id: 5, name: 'Captain America'},
+    {id: 6, name: 'Catwoman'},
+    {id: 7, name: 'Robin'},
+    {id: 8, name: 'Hulk'},
+    {id: 9, name: 'Ironman'},
+    {id: 10, name: 'Deadpool'},
+];
