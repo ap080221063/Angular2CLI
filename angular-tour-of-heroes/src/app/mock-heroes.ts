039ba9feb8510b762ef8abbdb8c53aa8bfd141aa
ap080221063 <ap080221063@gmail.com>
2018-02-24 01:53:28 +0000

    add hero images

--- angular-tour-of-heroes/src/app/mock-heroes.ts
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -1,14 +1,14 @@
 import {Hero} from './hero';
 
 export const HEROES: Hero[] = [
-    {id: 1,  image: 'Superman.jpg'       , name: 'Superman'       , description:'' },
-    {id: 2,  image: 'Batman.jpg'         , name: 'Batman'         , description:'' },
-    {id: 3,  image: 'Spiderman.jpg'      , name: 'Spiderman'      , description:'' },
-    {id: 4,  image: 'Thor.jpg'           , name: 'Thor'           , description:'' },
-    {id: 5,  image: 'Captain America.jpg', name: 'Captain America', description:'' },
-    {id: 6,  image: 'Catwoman.jpg'       , name: 'Catwoman'       , description:'' },
-    {id: 7,  image: 'Robin.jpg'          , name: 'Robin'          , description:'' },
-    {id: 8,  image: 'Hulk.jpg'           , name: 'Hulk'           , description:'' },
-    {id: 9,  image: 'Ironman.jpg'        , name: 'Ironman'        , description:'' },
-    {id: 10, image: 'Deadpool.jpg'       , name: 'Deadpool'       , description:'' },
+    {id: 1,  image: 'superman.jpg'       , name: 'Superman'       , description:'' },
+    {id: 2,  image: 'batman.jpg'         , name: 'Batman'         , description:'' },
+    {id: 3,  image: 'spiderman.jpg'      , name: 'Spiderman'      , description:'' },
+    {id: 4,  image: 'thor.jpg'           , name: 'Thor'           , description:'' },
+    {id: 5,  image: 'captainamerica.jpg' , name: 'Captain America', description:'' },
+    {id: 6,  image: 'catwoman.jpg'       , name: 'Catwoman'       , description:'' },
+    {id: 7,  image: 'robin.jpg'          , name: 'Robin'          , description:'' },
+    {id: 8,  image: 'hulk.jpg'           , name: 'Hulk'           , description:'' },
+    {id: 9,  image: 'ironman.jpg'        , name: 'Ironman'        , description:'' },
+    {id: 10, image: 'deadpool.jpg'       , name: 'Deadpool'       , description:'' },
 ];
